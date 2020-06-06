dc0398c2fdd46d43e856bad4230bc15f8b1a0196
qorlgns1 <rlgns0610@gmail.com>
2020-06-06 12:05:29 +0900

    0606

--- 포트폴리오/src/코딩연습/완주하지못한선수.java
+++ 포트폴리오/src/코딩연습/완주하지못한선수.java
@@ -2,4 +2,12 @@ package 코딩연습;
 
 public class 완주하지못한선수 {
 	
+	class Solution {
+	    public String solution(String[] participant, String[] completion) {
+	        String answer = "";
+	        return answer;
+	        
+	       
+	    }
+	}
 }
